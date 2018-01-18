31c8df40cae5c49a632bf9b14c3b2cb0fc27e4a3
Jim Mullen <mullen2@iu.edu>
2018-01-18 12:29:14 -0500

    Added initial configuration check script

---
+++ bin/config_check.php
@@ -0,0 +1,43 @@
+#!/usr/bin/env php
+<?php
+
+require(__DIR__.'/../dependencies/autoload.php');
+
+#-----------------------------------------------------------
+# Script for checking configuration file
+#
+# Arguments: <properties-file>
+#-----------------------------------------------------------
+
+use IU\REDCapETL\Configuration;
+use IU\REDCapETL\EtlException;
+use IU\REDCapETL\Logger2;
+
+
+if (count($argv) != 2) {
+    print "Usage: php $argv[0] <config-file>\n";
+    exit(1);
+} else {
+    $configFile = $argv[1];
+}
+
+try {
+    $app = basename(__FILE__, '.php');
+    $logger = new Logger2($app);
+
+    $configuration = new Configuration($logger, null, $configFile);
+    print "Configuration file {$configFile} is OK.\n";
+} catch (EtlException $exception) {
+    $cause = null;
+    $previous = $exception->getPrevious();
+    if (isset($previous)) {
+        $cause = $previous->getMessage();
+    }
+
+    if (isset($cause)) {
+        print "Error found: ".($exception->getMessage())."; cause: ".$cause."\n";
+    } else {
+        print "Error found: ".$exception->getMessage()."\n";
+    }
+}
+
