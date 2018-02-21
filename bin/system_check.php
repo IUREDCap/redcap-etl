acd0b1f8d431896f42e285cde9d60f904d155190
Jim Mullen <mullen2@iu.edu>
2018-02-21 16:39:10 -0500

    Added initial system check script

---
+++ bin/system_check.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+
+$minVersion = '5.6.0';
+
+print "PHP version ".PHP_VERSION." ";
+
+if (version_compare(PHP_VERSION, $minVersion) < 0) {
+    print "- ERROR: you need to be running at least PHP version ".$minVersion;
+} else {
+    print "- OK";
+}
+print "\n";
+
+
+if (in_array('curl', get_loaded_extensions())) {
+    print "curl enabled\n";
+} else {
+    print "curl disabled - ERROR: curl needs to be enabled in PHP\n";
+}
+
+if (in_array('openssl', get_loaded_extensions())) {
+    print "openssl enabled\n";
+} else {
+    print "openssl disabled - ERROR: openssl needs to be enabled in PHP\n";
+}
+
+if (function_exists('mail')) {
+    print "mail function exists\n";
+}
