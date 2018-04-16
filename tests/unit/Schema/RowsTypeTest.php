f8870c0c5b675b28ef3f52c904e937759ace7c00
Jim Mullen <mullen2@iu.edu>
2018-04-16 15:27:12 -0400

    Improved tests coverage

---
+++ tests/unit/Schema/RowsTypeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace IU\REDCapETL\Schema;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * PHPUnit tests for Row class.
+ */
+class RowsTypeTest extends TestCase
+{
+    public function testCreateRow()
+    {
+        $this->assertTrue(RowsType::isValid(RowsType::ROOT));
+        $this->assertTrue(RowsType::isValid(RowsType::BY_REPEATING_INSTRUMENTS));
+
+        $this->assertFalse(RowsType::isValid(1000));
+    }
+}
