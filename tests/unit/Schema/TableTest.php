f74b3d6c648942341f27c49f5282f5c797306bbd
Abhijeet Malatpure <abhmalat@iu.edu>
2018-05-15 14:30:26 -0400

    Added test cases for class Table. Covers 82 percent of code

--- tests/unit/Schema/TableTest.php
+++ tests/unit/Schema/TableTest.php
@@ -25,7 +25,8 @@ class TableTest extends TestCase
 
         $this->assertEquals($name, $table->getName(), 'getName test');
 
-        $childTable = new Table('child', $table, $keyType, RowsType::BY_EVENTS, '', $recordIdFieldName);
+        $suffixes = array(1, 2, 3);
+        $childTable = new Table('child', $table, $keyType, RowsType::BY_EVENTS, $suffixes , $recordIdFieldName);
         $this->assertNotNull($childTable, 'child table not null');
 
         $childTable->setForeign($table);
@@ -33,11 +34,74 @@ class TableTest extends TestCase
         //Create primary key for child table
         $childTable->createPrimary();
 
+        //Add childTable as a child of table
+        $table->addChild($childTable);
+
+        $child = $table->getChildren();
+        $this->assertNotNull($child, 'child for primary table not null');
+
         // Haven't added any rows, so count should be zero
         $rowsCount = $table->getNumRows();
         $this->assertEquals(0, $rowsCount);
 
-        #$expectedRowData = array('id' => 100, 'name' => 'Bob');
+        $table->addField(new Field('name',FieldType::VARCHAR,20));
+
+        //Add fields to child table
+        $fields = array(
+          new Field('biology',FieldType::INT,3),
+          new Field('chemistry',FieldType::INT,3)
+        );
+
+        foreach ($fields as $field) {
+          $childTable->addField($field);
+        }
+
+        //Create row for parent and child tables
+        $parentRows = array(
+          $table->createRow(array('name' => 'Bob'),'',''),
+          $table->createRow(array('name' => 'Tina'),'',''),
+          $table->createRow(array('name' => 'Mark'),'','')
+        );
+
+        foreach ($parentRows as $row) {
+          // code...
+          $table->addRow($row);
+        }
+
+        $this->assertCount(3, $table->getRows());
+
+        $childRows = array(
+          $childTable->createRow(array('chemistry' => 86, 'biology' => 93),1, ''),
+          $childTable->createRow(array('chemistry' => 75, 'biology' => 81),3, '')
+        );
+        foreach ($childRows as $row) {
+          // code...
+          $childTable->addRow($row);
+        }
+
+        $childTable->getPossibleSuffixes();
+        $table->getPossibleSuffixes();
+
+        // Child table should have 2 rows
+        $rowsCount = $childTable->getNumRows();
+        $this->assertEquals(4, $rowsCount);
+
+
+        $this->assertCount(4, $childTable->getRows());
+        #print_r($childRows);
+
+        //call toString function for both classes and print them
+        #$childTable->toString();
+
+        //This causes an error at line 342 in class Table
+        #print_r($table->toString());
+
+        #Child table should have a primary key, foreign key from parent table,
+        # and two more fields added above
+        $this->assertCount(4, $childTable->getAllFields());
+
+        $table->emptyRows();
+        #expectedRowData = array('id' => 100, 'name' => 'Bob');
         #$expectedRowString = " (id: 100, name: Bob)\n";
 
         #foreach ($expectedRowData as $fieldName => $value) {
