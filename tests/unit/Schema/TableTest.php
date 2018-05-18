c6f28100cd143241a23c9607f4c516c0f2ad7f29
Abhijeet Malatpure <abhmalat@iu.edu>
2018-05-18 08:16:20 -0700

    Unit Test cases for Table

--- tests/unit/Schema/TableTest.php
+++ tests/unit/Schema/TableTest.php
@@ -28,17 +28,17 @@ class TableTest extends TestCase
         $childTable = new Table('child', $table, $keyType, RowsType::BY_EVENTS, '', $recordIdFieldName);
         $this->assertNotNull($childTable, 'child table not null');
 
-        #$expectedRowData = array('id' => 100, 'name' => 'Bob');
-        #$expectedRowString = " (id: 100, name: Bob)\n";
+        $expectedRowData = array('id' => 100, 'name' => 'Bob');
+        $expectedRowString = " (id: 100, name: Bob)\n";
 
-        #foreach ($expectedRowData as $fieldName => $value) {
-        #    $row->addValue($fieldName, $value);
-        #}
+        foreach ($expectedRowData as $fieldName => $value) {
+            $row->addValue($fieldName, $value);
+        }
 
-        #$rowData = $row->getData();
-        #$this->assertEquals($expectedRowData, $rowData, 'row data check');
+        $rowData = $row->getData();
+        $this->assertEquals($expectedRowData, $rowData, 'row data check');
 
-        #$rowString = $row->toString(1);
-        #$this->assertEquals($expectedRowString, $rowString, 'row string check');
+        $rowString = $row->toString(1);
+        $this->assertEquals($expectedRowString, $rowString, 'row string check');
     }
 }
