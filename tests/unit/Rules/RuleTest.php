da6918be501a6c23dacf25c63b07ddaf6ae2eeb5
Jim Mullen <mullen2@iu.edu>
2018-05-30 08:32:53 -0400

    Fixed coding standards issues

--- tests/unit/Rules/RuleTest.php
+++ tests/unit/Rules/RuleTest.php
@@ -13,41 +13,38 @@ class RuleTest extends TestCase
     public function testCreateRule()
     {
 
-      // Test that a Rule can be created
-      $expectedLine = 'Test Rule String';
-      $expectedLineNumber = 37;
+        // Test that a Rule can be created
+        $expectedLine = 'Test Rule String';
+        $expectedLineNumber = 37;
 
-      $rule = new Rule($expectedLine, $expectedLineNumber);
-      $this->assertNotNull($rule, 'rule not null');
+        $rule = new Rule($expectedLine, $expectedLineNumber);
+        $this->assertNotNull($rule, 'rule not null');
 
-      // Test that the rule returns the expected line and lineNumber
-      $line = $rule->getLine();
-      $this->assertEquals($expectedLine, $line, 
-			  'line check');
+        // Test that the rule returns the expected line and lineNumber
+        $line = $rule->getLine();
+        $this->assertEquals($expectedLine, $line, 'line check');
 
-      $lineNumber = $rule->getLineNumber();
-      $this->assertEquals($expectedLineNumber, $lineNumber, 
-			  'lineNumber check');
+        $lineNumber = $rule->getLineNumber();
+        $this->assertEquals($expectedLineNumber, $lineNumber, 'lineNumber check');
 
 
-      // Test that the rule is empty of errors
-      $hasErrors = $rule->hasErrors();
-      $this->assertEquals(false, $hasErrors, 'No errors check');
-      $errors = $rule->getErrors();
-      $this->assertEquals(0, count($errors), 'No errors returned check');
+        // Test that the rule is empty of errors
+        $hasErrors = $rule->hasErrors();
+        $this->assertEquals(false, $hasErrors, 'No errors check');
+        $errors = $rule->getErrors();
+        $this->assertEquals(0, count($errors), 'No errors returned check');
 
 
-      // Test that the rule accepts and returns errors
-      $error1 = "First error";
-      $error2 = "Second error";
-      $expectedErrors = array($error1, $error2);
-      $rule->addError($error1);
-      $rule->addError($error2);
+        // Test that the rule accepts and returns errors
+        $error1 = "First error";
+        $error2 = "Second error";
+        $expectedErrors = array($error1, $error2);
+        $rule->addError($error1);
+        $rule->addError($error2);
 
-      $hasErrors = $rule->hasErrors();
-      $this->assertEquals(true, $hasErrors, 'Errors check');
-      $errors = $rule->getErrors();
-      $this->assertEquals($expectedErrors, $errors, 'Errors returned check');
-
+        $hasErrors = $rule->hasErrors();
+        $this->assertEquals(true, $hasErrors, 'Errors check');
+        $errors = $rule->getErrors();
+        $this->assertEquals($expectedErrors, $errors, 'Errors returned check');
     }
 }
