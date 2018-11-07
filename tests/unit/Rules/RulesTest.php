cd12c919e8a30dc6983ecd949e304d55b39ed341
Andy Arenson <aarenson@iu.edu>
2018-11-07 11:09:49 -0500

    psr-2 related whitespace changes

--- tests/unit/Rules/RulesTest.php
+++ tests/unit/Rules/RulesTest.php
@@ -12,13 +12,13 @@ use IU\REDCapETL\RedCapEtl;
  */
 class RulesTest extends TestCase
 {
-  
+
     public function testAddRules()
     {
         // Test that a Rules object can be created
         $rules = new Rules();
         $this->assertNotNull($rules, 'rules not null');
-        
+
 
         // Test that Rule objects can be added to a Rules object
         $parseableLine = 'Parseable Line';
@@ -29,14 +29,14 @@ class RulesTest extends TestCase
         $unparseableError = 'Unparseable Line is unparseable';
         $unparseableRule = new Rule($parseableLine, $parseableLineNumber);
         $unparseableRule->addError($unparseableError);
-        
+
         $rules->addRule($parseableRule);
         $rules->addRule($unparseableRule);
-        
+
         $retrievedRules = $rules->getRules();
         $this->assertEquals(2, count($retrievedRules), 'Rules added check');
-        
-        
+
+
         // Test that the parsedLineCount can be retrieved
         $parsedLineCount = $rules->getParsedLineCount();
         $this->assertEquals(1, $parsedLineCount, 'ParsedLineCount check');
