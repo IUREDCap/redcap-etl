a48fe8fa8d59e94c916c9c50a62ca0b3d58c31e3
Andy Arenson <aarenson@iu.edu>
2018-11-08 09:32:52 -0500

    Add unit test for TableRule.php

---
+++ tests/unit/Rules/TableRuleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace IU\REDCapETL\Rules;
+
+use PHPUnit\Framework\TestCase;
+use IU\REDCapETL\RedCapEtl;
+
+/**
+ * PHPUnit tests for the Rule class.
+ */
+class TableRuleTest extends TestCase
+{
+    public function testCreateRule()
+    {
+
+        // Test that a Rule can be created
+        $expectedLine = 'Test Table Rule String';
+        $expectedLineNumber = 37;
+
+        $rule = new TableRule($expectedLine, $expectedLineNumber);
+        $this->assertNotNull($rule, 'rule not null');
+
+        // Test that the rule returns the expected line, as a stand-in
+        // for testing that the TableRule was created
+        $line = $rule->getLine();
+        $this->assertEquals($expectedLine, $line, 'line check');
+    }
+}
