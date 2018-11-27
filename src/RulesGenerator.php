169cb2a9ebf18693b5f8fa4a0870dbc2ea2000c0
Ryan Long <ryanlong@iu.edu>
2018-11-27 11:32:09 -0500

    Added two more projects to the RulesGenerator test

--- src/RulesGenerator.php
+++ src/RulesGenerator.php
@@ -36,18 +36,24 @@ class RulesGenerator
         # Get project information and metadata
         #----------------------------------------------------
         $projectInfo = $dataProject->exportProjectInfo();
-        // echo "PROJECT INFO: ";
-        // echo json_encode($projectInfo);
+        echo "{ \"projectInfo\": ";
+        echo json_encode($projectInfo, JSON_PRETTY_PRINT);
         $this->isLongitudinal = $projectInfo['is_longitudinal'];
         $this->instruments = $dataProject->exportInstruments();
-        // echo "INSTRUMENT INFO: ";
-        // echo json_encode($this->instruments);
+        echo ", \"instruments\": ";
+        echo json_encode($this->instruments, JSON_PRETTY_PRINT);
         $this->metadata    = $dataProject->exportMetadata();
-        // echo "METADATA INFO: ";
-        // echo json_encode($this->metadata);
+        echo ", \"metadata\": ";
+        echo json_encode($this->metadata, JSON_PRETTY_PRINT);
+        echo "}";
+        echo "\n";
+        echo "\n";
         $projectXml  = $dataProject->exportProjectXml($metadataOnly = true);
-        // echo "PROJECTXML INFO: ";
-        // echo $projectXml;
+        echo "\n";
+        echo "\n";
+        echo $projectXml;
+        echo "\n";
+        echo "\n";
         $this->projectXmlDom = new \DomDocument();
         $this->projectXmlDom->loadXML($projectXml);
 
@@ -55,14 +61,17 @@ class RulesGenerator
 
         if ($this->isLongitudinal) {
             $this->eventMappings = $dataProject->exportInstrumentEventMappings();
-            // echo "EVENT MAPPINGS: ";
-            // echo json_encode($this->eventMappings);
+            echo "\n";
+            echo "\n \"instrumentEventMappings\": ";
+            echo json_encode($this->eventMappings, JSON_PRETTY_PRINT);
+            echo "\n";
+            echo "\n";
             $rules = $this->generateLongitudinalProjectRules();
         } else {
             $rules = $this->generateClassicProjectRules();
         }
 
-        // print_r($rules);
+        print_r($rules);
         return $rules;
     }
     
--- tests/unit/RulesGeneratorTest.php
+++ tests/unit/RulesGeneratorTest.php
@@ -186,4 +186,126 @@ class RulesGeneratorTest extends TestCase
 
         $this->assertSame($expectedResult, $rulesText);
     }
+
+    public function testGenerateRepeatingEventAndInst()
+    {
+        $jsonFile  = __DIR__.'/../data/repeating-event-inst.json';
+        $xmlFile   = __DIR__.'/../data/repeating-event-inst.xml';
+        $rulesFile = __DIR__.'/../data/repeating-event-inst.txt';
+
+        $projectData = new ProjectData($jsonFile, $xmlFile, $rulesFile);
+
+        $projectInfo = $projectData->getProjectInfo();
+        $instruments = $projectData->getInstruments();
+        $metadata    = $projectData->getMetadata();
+        $projectXml  = $projectData->getProjectXml();
+
+        $eventMappings = $projectData->getInstrumentEventMappings();
+
+        $expectedResult = $projectData->getRulesText();
+
+        $dataProject = $this->getMockBuilder(__NAMESPACE__.'EtlRedCapProject')
+            ->setMethods(
+                [
+                    'exportProjectInfo',
+                    'exportInstruments',
+                    'exportMetadata',
+                    'exportProjectXml',
+                    'exportInstrumentEventMappings'
+                ]
+            )
+            ->getMock();
+
+
+        // exportProjectInfo() - stub method returning mock data
+        $dataProject->expects($this->any())
+            ->method('exportProjectInfo')
+            ->will($this->returnValue($projectInfo));
+
+        // exportInstruments() - stub method returning mock data
+        $dataProject->expects($this->any())
+            ->method('exportInstruments')
+            ->will($this->returnValue($instruments));
+
+        // exportMetadata() - stub method returning mock data
+        $dataProject->expects($this->any())
+        ->method('exportMetadata')
+        ->will($this->returnValue($metadata));
+
+        // exportProjectXml() - stub method returning mock data
+
+        $dataProject->expects($this->any())
+        ->method('exportProjectXml')
+        ->will($this->returnValue($projectXml));
+    
+        $dataProject->expects($this->any())
+        ->method('exportInstrumentEventMappings')
+        ->will($this->returnValue($eventMappings));
+
+        $rulesGenerator = new RulesGenerator();
+        $rulesText = $rulesGenerator->generate($dataProject);
+
+        $this->assertSame($expectedResult, $rulesText);
+    }    
+
+    public function testGenerateNonRepeating()
+    {
+        $jsonFile  = __DIR__.'/../data/non-repeating.json';
+        $xmlFile   = __DIR__.'/../data/non-repeating.xml';
+        $rulesFile = __DIR__.'/../data/non-repeating.txt';
+
+        $projectData = new ProjectData($jsonFile, $xmlFile, $rulesFile);
+
+        $projectInfo = $projectData->getProjectInfo();
+        $instruments = $projectData->getInstruments();
+        $metadata    = $projectData->getMetadata();
+        $projectXml  = $projectData->getProjectXml();
+
+        $eventMappings = $projectData->getInstrumentEventMappings();
+
+        $expectedResult = $projectData->getRulesText();
+
+        $dataProject = $this->getMockBuilder(__NAMESPACE__.'EtlRedCapProject')
+            ->setMethods(
+                [
+                    'exportProjectInfo',
+                    'exportInstruments',
+                    'exportMetadata',
+                    'exportProjectXml',
+                    'exportInstrumentEventMappings'
+                ]
+            )
+            ->getMock();
+
+
+        // exportProjectInfo() - stub method returning mock data
+        $dataProject->expects($this->any())
+            ->method('exportProjectInfo')
+            ->will($this->returnValue($projectInfo));
+
+        // exportInstruments() - stub method returning mock data
+        $dataProject->expects($this->any())
+            ->method('exportInstruments')
+            ->will($this->returnValue($instruments));
+
+        // exportMetadata() - stub method returning mock data
+        $dataProject->expects($this->any())
+        ->method('exportMetadata')
+        ->will($this->returnValue($metadata));
+
+        // exportProjectXml() - stub method returning mock data
+
+        $dataProject->expects($this->any())
+        ->method('exportProjectXml')
+        ->will($this->returnValue($projectXml));
+    
+        $dataProject->expects($this->any())
+        ->method('exportInstrumentEventMappings')
+        ->will($this->returnValue($eventMappings));
+
+        $rulesGenerator = new RulesGenerator();
+        $rulesText = $rulesGenerator->generate($dataProject);
+
+        $this->assertSame($expectedResult, $rulesText);
+    }
 }
