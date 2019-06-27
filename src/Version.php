41f240bc1a0b4813fe2913983b6ed27b0756fb24
Jim Mullen <mullen2@iu.edu>
2019-06-27 08:00:36 -0400

    Updated version release number to 0.7.6

--- src/Version.php
+++ src/Version.php
@@ -10,5 +10,5 @@ class Version
     # This release number should be updated each time a release is made.
     # This value will be logged to indicate which version of the
     # software is being used.
-    const RELEASE_NUMBER = "0.7.5";
+    const RELEASE_NUMBER = "0.7.6";
 }
