1e88776fd8bfd2f0205adf5806acd31ff018466d
N Long <naalong@iu.edu>
2019-07-29 17:17:08 -0400

    MySqlDbConnection, PdoMySqlDbConnection, and EtlEventLogTable tests

--- src/Database/PdoMysqlDbConnection.php
+++ src/Database/PdoMysqlDbConnection.php
@@ -40,12 +40,13 @@ class PdoMysqlDbConnection extends DbConnection
         } else {
             $message = 'The database connection is not correctly formatted: ';
             if (count($dbValues) < 4) {
-                $message = 'not enough values.';
+                $message .= 'not enough values.';
             } else {
-                $message = 'too many values.';
+                $message .= 'too many values.';
             }
             $code = EtlException::DATABASE_ERROR;
-            throw new \Exception($message, $code);
+            #throw new Exception($message, $code);
+            throw new EtlException($message, $code);
         }
 
         if (empty($port)) {
---
+++ tests/unit/EtlEventLogTableTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace IU\REDCapETL;
+
+use PHPUnit\Framework\TestCase;
+
+use IU\REDCapETL\Schema\Field;
+
+/**
+* PHPUnit tests for the EtlEventLogTable class
+*/
+
+class EtlEventLogTableTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $name = "etlEventLog";
+
+        #test object creation
+        $etlEventLogTable = new EtlEventLogTable($name);
+        $this->assertNotNull($etlEventLogTable, 'etlEventLogTable object not null check');
+
+        #test table name correctly
+        $this->assertEquals($name, $etlEventLogTable->name, 'etlEventLogTable name check');
+
+        #test fields added correctly
+        $expectedCount = 3;
+        $expectedName = ['log_id','time','message'];
+        $expectedType = ['int','datetime','string'];
+        $i = 0;
+        $fieldsOk = true;
+        foreach ($etlEventLogTable->getFields() as $field) {
+            if (($field->name !== $expectedName[$i]) || ($field->type !== $expectedType[$i])) {
+                $fieldsOk = false;
+            }
+            $i++;
+        }
+        $this->assertEquals($expectedCount, $i, 'etlEventLogTable field count check');
+        $this->assertTrue($fieldsOk, 'etlEventLogTable fields ok check');
+    }
+
+    public function xcreateEventLogDataRowTest()
+    {
+        $name = "etlEventLog";
+        $etlEventLogTable = new EtlEventLogTable($name);
+
+        $logId = 123;
+        $message = 'This is an event log message.';
+        $result = $etlEventLogTable->createEventLogDataRow($logId, $message);
+        $this->assertNull($result, 'addLookupField return check');
+        print "printing row: ";
+        print_r($result);
+    }
+}
