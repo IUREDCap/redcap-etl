79a7faa8f33fb0907e45add00cd5517f25a3db36
N Long <naalong@iu.edu>
2019-07-31 16:50:29 -0400

    EtlLogData and EtlEventLogDataTests

--- tests/unit/EtlEventLogTableTest.php
+++ tests/unit/EtlEventLogTableTest.php
@@ -39,16 +39,29 @@ class EtlEventLogTableTest extends TestCase
         $this->assertTrue($fieldsOk, 'etlEventLogTable fields ok check');
     }
 
-    public function createEventLogDataRowTest()
+    public function testCreateEventLogDataRowTest()
     {
-        $name = "etlEventLog";
-        $etlEventLogTable = new EtlEventLogTable($name);
+        $now = time();
+        $tableName = "etlEventLog";
+        $etlEventLogTable = new EtlEventLogTable($tableName);
 
         $logId = 123;
         $message = 'This is an event log message.';
+
+        $expectedTableParent = 'log_event_id';
+        $expectedPrimaryName = 'log_event_id';
+        $expectedPrimaryType = 'auto_increment';
+        $expectedPrimaryDbName = 'log_event_id';
+
         $result = $etlEventLogTable->createEventLogDataRow($logId, $message);
-        $this->assertNull($result, 'addLookupField return check');
-        print "printing row: ";
-        print_r($result);
+
+        $this->assertEquals($tableName, $result->table->name, 'testCreateEventLogDataRow table name check');
+        $this->assertEquals($expectedTableParent, $result->table->parent, 'testCreateEventLogDataRow table parent check');
+        $this->assertEquals($expectedPrimaryName, $result->table->primary->name, 'testCreateEventLogDataRow primary name check');
+        $this->assertEquals($expectedPrimaryType, $result->table->primary->type, 'testCreateEventLogDataRow primary type check');
+        $this->assertEquals($expectedPrimaryDbName, $result->table->primary->dbName, 'testCreateEventLogDataRow primary dbName check');
+        $this->assertEquals($logId, $result->data['log_id'], 'testCreateEventLogDataRow data1 check');
+        $this->assertGreaterThanOrEqual($now, strtotime($result->data['time']), 'testCreateEventLogDataRow data2 check');
+        $this->assertEquals($message, $result->data['message'], 'testCreateEventLogDataRow data3 check');
     }
 }
---
+++ tests/unit/EtlLogTableTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace IU\REDCapETL;
+
+use PHPUnit\Framework\TestCase;
+
+use IU\REDCapETL\Schema\Field;
+
+/**
+* PHPUnit tests for the EtlLogTable class
+*/
+
+class EtlLogTableTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $name = "etlLog";
+
+        #test object creation
+        $etlLogTable = new EtlLogTable($name);
+        $this->assertNotNull($etlLogTable, 'etlLogTable object not null check');
+
+        #test table name correctly
+        $this->assertEquals($name, $etlLogTable->name, 'etlLogTable name check');
+
+        #test fields added correctly
+        $expectedCount = 8;
+        $expectedSize = [6, 60,60,'',10,40,40,''];
+        $expectedName = [
+            'start_time'
+            ,'app'
+            ,'table_prefix'
+            ,'batch_size'
+            ,'redcap_etl_version'
+            ,'php_version'
+            ,'timezone'
+            ,'utc_offset'
+        ];
+        $expectedType = [
+            'datetime'
+            ,'varchar'
+            ,'varchar'
+            ,'int'
+            ,'char'
+            ,'varchar'
+            ,'varchar'
+            'int'
+        ];
+        $expectedSize = [6, 60,60,'',10,40,40,''];
+        $i = 0;
+        $fieldsOk = true;
+        foreach ($etlLogTable->getFields() as $field) {
+            if (($field->name !== $expectedName[$i])
+                || ($field->type !== $expectedType[$i])
+                || ($field->size != $expectedSize[$i])) {
+                $fieldsOk = false;
+            }
+            $i++;
+        }
+        $this->assertEquals($expectedCount, $i, 'etlLogTable field count check');
+        $this->assertTrue($fieldsOk, 'etlLogTable fields ok check');
+    }
+
+    public function testCreateLogDataRowTest()
+    {
+        $now = time();
+        $tableName = "etlLog2";
+        $etlLogTable = new EtlLogTable($tableName);
+
+        $app = "app1";
+        $tablePrefix = "helpMe";
+        $batchSize = "333";
+        $result = $etlLogTable->createLogDataRow($app, $tablePrefix, $batchSize);
+        $this->assertNotNull($result, 'testCreateLogDataRow return check');
+
+        $expectedTableParent = 'log_id';
+        $expectedPrimaryName = 'log_id';
+        $expectedPrimaryDbName = 'log_id';
+        $expectedPrimaryType = 'auto_increment';
+
+        $this->assertEquals($tableName, $result->table->name, 'testCreateLogDataRow table name check');
+        $this->assertEquals($expectedTableParent, $result->table->parent, 'testCreateLogDataRow table parent check');
+        $this->assertEquals($expectedPrimaryName, $result->table->primary->name, 'testCreateLogDataRow primary name check');
+        $this->assertEquals($expectedPrimaryType, $result->table->primary->type, 'testCreateLogDataRow primary type check');
+        $this->assertEquals($expectedPrimaryDbName, $result->table->primary->dbName, 'testCreateLogDataRow primary dbName check');
+        $this->assertGreaterThanOrEqual($now, strtotime($result->data['start_time']), 'testCreateLogDataRow data1 check');
+        $this->assertEquals($app, $result->data['app'], 'testCreateLogDataRow data2 check');
+        $this->assertEquals($tablePrefix, $result->data['table_prefix'], 'testCreateLogDataRow data3 check');
+        $this->assertEquals($batchSize, $result->data['batch_size'], 'testCreateLogDataRow data4 check');
+        $this->assertNotNull($result->data['redcap_etl_version'], 'testCreateLogDataRow data5 check');
+        $this->assertNotNull($result->data['php_version'], 'testCreateLogDataRow data6 check');
+        $this->assertNotNull($result->data['timezone'], 'testCreateLogDataRow data7 check');
+        $this->assertNotNull($result->data['utc_offset'], 'testCreateLogDataRow data8 check');
+    }
+}
