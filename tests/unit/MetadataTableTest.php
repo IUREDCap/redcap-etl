5214cc70eae8e388752fdf3f20c3314a058fa634
Jim Mullen <mullen2@iu.edu>
2021-05-28 10:15:53 -0400

    Added automated tests

--- tests/unit/MetadataTableTest.php
+++ tests/unit/MetadataTableTest.php
@@ -27,4 +27,28 @@ class MetadataTableTest extends TestCase
 
         $this->assertEquals($name, $metadataTable->getName(), 'MetadataTable name check');
     }
+
+    public function testMerge()
+    {
+        $name1 = 'metadata1';
+        $name2 = 'metadata2';
+
+        $metadataTable1 = new MetadataTable($name1);
+        $this->assertNotNull($metadataTable1, 'metadataTable1 object not null check');
+        $this->assertEquals($name1, $metadataTable1->getName(), 'metadataTable1 name check');
+
+        $metadataTable2 = new MetadataTable($name2);
+        $this->assertNotNull($metadataTable2, 'metadataTable2 object not null check');
+        $this->assertEquals($name2, $metadataTable2->getName(), 'metadataTable2 name check');
+
+        $exceptionCaught = false;
+        try {
+            $metadataTable1->merge($metadataTable2);
+        } catch (\Exception $exception) {
+            $exceptionCaught = true;
+            $message = $exception->getMessage();
+        }
+        $this->assertTrue($exceptionCaught, 'Exception caught');
+        $this->assertStringContainsString('do not match', $message, 'Exception message');
+    }
 }
