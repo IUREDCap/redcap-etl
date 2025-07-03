ebd1d5d38641aef6f645922cbb8c81dfab4a91b3
Jim Mullen <mullen2@iu.edu>
2025-07-03 09:29:24 -0400

    Updated version release number to 2.3.15

--- src/Version.php
+++ src/Version.php
@@ -1,6 +1,6 @@
 <?php
 #-------------------------------------------------------
-# Copyright (C) 2019 The Trustees of Indiana University
+# Copyright (C) 2025 The Trustees of Indiana University
 # SPDX-License-Identifier: BSD-3-Clause
 #-------------------------------------------------------
 
@@ -14,5 +14,5 @@ class Version
     # This release number should be updated each time a release is made.
     # This value will be logged to indicate which version of the
     # software is being used.
-    const RELEASE_NUMBER = "2.3.14";
+    const RELEASE_NUMBER = "2.3.15";
 }
